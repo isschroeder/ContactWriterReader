cfe770d24ded5ac9e84c3f047d335b2a2df63e6e
isschroeder <kittypuppybird@gmail.com>
2015-12-13 22:51:01 -0500

    fixed loopie loop?
    still won't work. much, much sadness

--- src/ContactWriterReader.java
+++ src/ContactWriterReader.java
@@ -22,23 +22,21 @@ public class ContactWriterReader {
 		File fileStuff = new File("myContacts.txt");
 		Contact[] myContacts;
 		myContacts = new Contact[110]; // initialize to 110 elements (enough for 10 contacts)
-		PrintWriter outputFile;
-		BufferedReader inputFile;
-		int numContacts = 1;
+		//PrintWriter outputFile;
+	//	BufferedReader inputFile;
+		int numContacts = 0;
 		Scanner keyboard = new Scanner(System.in);
 		String input;
 		int ctr = 1;
-		inputFile = new BufferedReader( new FileReader(fileStuff) );
-		outputFile = new PrintWriter(fileStuff);
+	//	inputFile = new BufferedReader( new FileReader(fileStuff) );
+	//	outputFile = new PrintWriter(fileStuff);
 		
 		if( fileStuff.exists() ){
 			BufferedReader reader = new BufferedReader( new FileReader(fileStuff) );
 			
-			while(reader.ready() ){
+			while(reader.ready() );
 				// create new contact objects
 				myContacts[numContacts] = new Contact();
-			
-				System.out.println( "Contact" + ctr );
 			
 				input = reader.readLine();
 				myContacts[numContacts].setLastName( input );
@@ -73,85 +71,75 @@ public class ContactWriterReader {
 				input = reader.readLine();
 				myContacts[numContacts].setOccupation( input );
 				// increment counter
-				ctr++;
-			}
-		reader.close();
-		}
-		for ( ctr = 1; ctr < numContacts; ctr++ ){
-			System.out.println( "Last name: " + myContacts[ctr].getLastName() );
-			System.out.println( "First name: " + myContacts[ctr].getFirstName() );
-			System.out.println( "Middle name: " + myContacts[ctr].getMiddleName() );
-			System.out.println( "Prefix: " + myContacts[ctr].getPrefix() );
-			System.out.println( "Phone: " + myContacts[ctr].getPhone() );
-			System.out.println( "Email: " + myContacts[ctr].getEmail() );
-			System.out.println( "Street: " + myContacts[ctr].getStreet() );
-			System.out.println( "City: " + myContacts[ctr].getCity() );
-			System.out.println( "State: " + myContacts[ctr].getState() );
-			System.out.println( "ZIP: " + myContacts[ctr].getZip() );
-			System.out.println( "Occupation: " + myContacts[ctr].getOccupation() );
-			System.out.println( "------" );
-		}
+				numContacts++;
+			
+			reader.close();
 		
-		do { System.out.println(fileStuff.getName() + 
-			" does not exist. Enter new contact information.");
-			fileStuff.createNewFile();
+			for ( ctr = 1; ctr < numContacts; ctr++ ){
+				System.out.println( "Last name: " + myContacts[ctr].getLastName() );
+				System.out.println( "First name: " + myContacts[ctr].getFirstName() );
+				System.out.println( "Middle name: " + myContacts[ctr].getMiddleName() );
+				System.out.println( "Prefix: " + myContacts[ctr].getPrefix() );
+				System.out.println( "Phone: " + myContacts[ctr].getPhone() );
+				System.out.println( "Email: " + myContacts[ctr].getEmail() );
+				System.out.println( "Street: " + myContacts[ctr].getStreet() );
+				System.out.println( "City: " + myContacts[ctr].getCity() );
+				System.out.println( "State: " + myContacts[ctr].getState() );
+				System.out.println( "ZIP: " + myContacts[ctr].getZip() );
+				System.out.println( "Occupation: " + myContacts[ctr].getOccupation() );
+				System.out.println( "------" );
+			}
+			
+		} else { 
+			//do { System.out.println(fileStuff.getName() + 
+			//" does not exist. Enter new contact information.");
+			//fileStuff.createNewFile();
 			
 			do{ myContacts[numContacts] = new Contact();
 			
 				System.out.print( "Last name: ");
 				input = keyboard.nextLine();
 				myContacts[numContacts].setLastName( input );
-				outputFile.println( myContacts[ctr].getLastName());
 			
 				System.out.print( "First name: ");
 				input = keyboard.nextLine();
 				myContacts[numContacts].setFirstName( input );
-				outputFile.println( myContacts[ctr].getFirstName());
 	   
 				System.out.print( "Middle name: " );
 				input = keyboard.nextLine();
 				myContacts[numContacts].setMiddleName( input );
-				outputFile.println( myContacts[ctr].getMiddleName());
 	        
 				System.out.print( "Prefix: " );
 				input = keyboard.nextLine();
 				myContacts[numContacts].setPrefix( input );
-				outputFile.println( myContacts[ctr].getPrefix());
 	        
 				System.out.print( "Phone number: " );
 				input = keyboard.nextLine();
 				myContacts[numContacts].setPhone( input );
-				outputFile.println( myContacts[ctr].getPhone());
 	        
 				System.out.print( "Email: " );
 				input = keyboard.nextLine();
 				myContacts[numContacts].setEmail( input );
-				outputFile.println( myContacts[ctr].getEmail());
 	        
 				System.out.print( "Street address: " );
 				input = keyboard.nextLine();
 				myContacts[numContacts].setStreet( input );
-				outputFile.println( myContacts[ctr].getStreet());
-	        
+
 				System.out.print( "City: " );
 				input = keyboard.nextLine();
 				myContacts[numContacts].setCity( input );
-				outputFile.println( myContacts[ctr].getCity());
 	        
 				System.out.print( "State: " );
 				input = keyboard.nextLine();
 				myContacts[numContacts].setState( input );
-				outputFile.println( myContacts[ctr].getState());
 	        
 				System.out.print( "ZIP code: " );
 				input = keyboard.nextLine();
 				myContacts[numContacts].setZip( input );
-				outputFile.println( myContacts[ctr].getZip());
 	        
 				System.out.print( "Occupation: " );
 				input = keyboard.nextLine();
 				myContacts[numContacts].setOccupation( input );
-				outputFile.println( myContacts[ctr].getOccupation());
 				
 				System.out.println( "Would you like to enter another(y/n)?" );
 				input = keyboard.nextLine().toLowerCase();
@@ -159,13 +147,39 @@ public class ContactWriterReader {
 				// increment our counter so we make a new object
 				numContacts++;
 			
-				}	while( input.equals("y") );
-				//writer = new PrinterWriter(fileStuff);
+				} while( input.equalsIgnoreCase("y") );
 				// loop through array and print
-			
-			outputFile.close();
-			
-		} while( !fileStuff.exists() );
+		}	
+		
+		PrintWriter writer = new PrintWriter( fileStuff );
+		
+		for ( ctr = 1; ctr < numContacts; ctr++ ){
+			writer.println( myContacts[ctr].getLastName() );
+			writer.println( myContacts[ctr].getFirstName() );
+			writer.println( myContacts[ctr].getMiddleName() );
+			writer.println( myContacts[ctr].getPrefix() );
+			writer.println( myContacts[ctr].getPhone() );
+			writer.println( myContacts[ctr].getEmail() );
+			writer.println( myContacts[ctr].getStreet() );
+			writer.println( myContacts[ctr].getCity() );
+			writer.println( myContacts[ctr].getState() );
+			writer.println( myContacts[ctr].getZip() );
+			writer.println( myContacts[ctr].getOccupation() );
+			System.out.println( "------" );
+		}
+		
+		writer.close();
+		
+		
+		
+		
+		
+		
+		//outputFile.close();
+		
+		
+		
+		/* while( !fileStuff.exists() );
 			input = keyboard.nextLine();
 		
 		inputFile.mark(256);
@@ -183,6 +197,8 @@ public class ContactWriterReader {
 		newStream.forEach( s -> System.out.println(s) );
 		
 		inputFile.close();
+		*/
+		
 	}
 
 }
